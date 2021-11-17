0a2cd7e813918ee7059fafdffa9a54b3f6b2dcda
Stephen <stephenjennings97@gmail.com>
2021-11-17 05:28:53 -0500

    Front end UI code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from './book'
+import { BookService } from './book.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'libraryWebAppFrontEnd';
+
+export class AppComponent implements OnInit {
+  public books: Book[];
+
+  constructor(private bookService: BookService){}
+
+  ngOnInit() {
+    this.getBooks();
+  }
+
+  public getBooks(): void {
+    this.bookService.getBooks().subscribe(
+      (response: Book[]) => {
+        this.books = response;
+        console.log(this.books);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
 }
